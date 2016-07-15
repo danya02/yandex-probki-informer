d5519c8b9c9df01997a6b60c413520213e23cab8
Danya Generalov <danya02@me.com>
2016-07-15 19:25:31 +0300

    Adding script to acquire traffic status as a 3-way semaphore.

---
+++ acquirer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import pygame
+import pygame.image
+import os
+import time
+# global declarations
+global color_table
+color_table = {
+    "green": pygame.Color(0, 154, 34, 255),
+    "yellow": pygame.Color(255, 102, 0, 255),
+    "red": pygame.Color(255, 0, 0, 255)
+}
+
+
+def download(path="/tmp/img.png",
+             url="http://info.maps.yandex.net/traffic/moscow/tends_200.png"):
+    os.popen("wget -O "+path+" "+url).read()
+
+
+def get_pix(path="/tmp/img.png", pix=(46, 67)):
+    download()
+    img = pygame.image.load(path)
+    global color_table
+    for i in color_table:
+        if img.get_at(pix) == color_table[i]:
+            return i
+    raise ValueError("Unexpected pixel at position "+str(pix)+": "+str(
+        img.get_at(pix)))
+
+
+def log():
+    out = open("/tmp/.last_traffic", "w")
+    out.write(str(time.time())+":"+get_pix())
+
+if __name__ == "__main__":
+    log()
