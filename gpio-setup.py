c834076ea42a8d7eb795ca0a6bb7cb00c2b1a868
Danya Generalov <danya02@me.com>
2016-07-29 21:23:17 +0300

    Adding license information to .py files.

--- gpio-setup.py
+++ gpio-setup.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+#    gpio-setup.py - a friendly-ish configuration interface for GPIO pins on a Raspberry Pi.
+#    Copyright (C) 2016 Danya Generalov
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import RPi.GPIO as gpio
 import threading
 import json
